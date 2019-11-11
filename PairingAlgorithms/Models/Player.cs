3c9650696ae7877b6d44abeba8d456354161da91
TarasekM <rezkth@gmail.com>
2019-11-11 11:13:39 +0100

    Make folders for classes

---
+++ PairingAlgorithms/Models/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatformForChessMatchmaking.Models { 
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Score { get; set; }
+
+        public int TotalGames { get; set; }
+        public int GamesAsWhite { get; set; }
+        public int GamesAsBlack { get; set; }
+
+        public bool HaveOponent { get; set; }
+        public int CurrentOpponentID { get; set; }
+
+        public Player(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            Score = 0;
+            TotalGames = 0;
+            GamesAsWhite = 0;
+            GamesAsBlack = 0;
+            HaveOponent = false;
+        }
+
+        override public string ToString()
+        {
+            return ID + ". " + Name; 
+        }
+    }
+}
---
+++ PairingAlgorithms/Models/Tournament.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlatformForChessMatchmaking.Algorithms;
+
+namespace PlatformForChessMatchmaking.Models
+{
+    public class Tournament
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public Pairing PairingSystem { get; set; }
+        public List<Player> Players { get; set; }
+
+        public Tournament(PairingFactory.PairingSystems PairingSystem)
+        {
+            this.PairingSystem = PairingFactory.GetPairing(PairingSystem, Players);
+        }
+
+        public void Pair()
+        {
+            PairingSystem.Pair();
+        }
+    }
+}
