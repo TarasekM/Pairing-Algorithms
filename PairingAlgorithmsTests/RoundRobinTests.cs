6ee5cf13975a10057c4f99971174a38f2eca3fa2
TarasekM <rezkth@gmail.com>
2019-11-11 15:39:32 +0100

    Add unit tests project to solution

---
+++ PairingAlgorithmsTests/RoundRobinTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using PairingAlgorithms.Models;
+using PairingAlgorithms.Systems;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
