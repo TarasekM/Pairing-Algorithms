dda0704bc97120905b8e434438283e11b7b0ce46
TarasekM <rezkth@gmail.com>
2019-11-16 12:20:58 +0100

    Make turnament abstract. Add abstract methods

--- PairingAlgorithms/Models/Tournament.cs
+++ PairingAlgorithms/Models/Tournament.cs
@@ -3,23 +3,29 @@ using PairingAlgorithms.Systems;
 
 namespace PairingAlgorithms.Models
 {
-    public class Tournament //TODO Make it abstract || Add methods like printScoreboard / remove Players etc.
+    public abstract class Tournament //TODO Make it abstract || Add methods like printScoreboard / remove Players etc.
     {
         public string Name { get; set; }
         public string Date { get; set; }
         public Pairing PairingSystem { get; set; }
         public List<Player> Players { get; set; }
 
-        public Tournament(PairingFactory.PairingSystems PairingSystem)
+        public Tournament(string Name, string Date, PairingFactory.PairingSystems PairingSystem)
         {
             this.PairingSystem = PairingFactory.GetPairing(PairingSystem);
-            this.Name = "Name not given";
-            this.Date = "Date not given";
+            this.Name = Name;
+            this.Date = Date;
         }
 
         public void Pair()
         {
-            PairingSystem.Pair(Players);
+            List<List<Player>> Pairings = PairingSystem.Pair(Players);
         }
+
+        public abstract void GetScoreboard();
+
+        public abstract void RemovePlayer();
+
+        public abstract void AddPlayer();
     }
 }
