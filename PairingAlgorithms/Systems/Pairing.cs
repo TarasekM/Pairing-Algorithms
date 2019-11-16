c029e4c4b634ac7dee905a3dc863e237213f9e2f
TarasekM <rezkth@gmail.com>
2019-11-16 12:09:52 +0100

    Delete unused variable

--- PairingAlgorithms/Systems/Pairing.cs
+++ PairingAlgorithms/Systems/Pairing.cs
@@ -5,9 +5,7 @@ namespace PairingAlgorithms.Systems
 {
     public abstract class Pairing : IPairing
     {
-        public List<Player> Players;
         public int CurrentRound { get; set; }
-
 
         public abstract List<List<Player>> Pair(List<Player> Players);
     }
