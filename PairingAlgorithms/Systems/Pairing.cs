3f4662c232fcc03230468b9cc9ddbd4cfa252605
unknown <m.tarasek@brainhint.com>
2019-12-29 18:06:12 +0100

    Add field MaxRound

--- PairingAlgorithms/Systems/Pairing.cs
+++ PairingAlgorithms/Systems/Pairing.cs
@@ -6,6 +6,7 @@ namespace PairingAlgorithms.Systems
     public abstract class Pairing : IPairing
     {
         public int CurrentRound { get; set; }
+        public int MaxRound { get; set; }
 
         public abstract List<List<int>> Pair(List<Player> Players);
     }
