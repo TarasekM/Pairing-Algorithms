b6a730413a722a23396736f7793b73c34cc1e24f
unknown <m.tarasek@brainhint.com>
2019-12-29 18:54:45 +0100

    Add Parent class for pairing systems tests and move common methods to it

---
+++ PairingAlgorithmsTests/PairingTest.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using PairingAlgorithms.Systems;
+using PairingAlgorithms.Models;
+namespace Tests{
+
+    public class PairingTest
+    {
+    protected List<Player> SetUpPlayers(int n)
+        {
+            List<Player> Players = new List<Player>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Players.Add(new Player(i, "Zawodnik nr " + i));
+            }
+
+            return Players;
+         }
+         
+        protected List<List<int>> SetUp_Pairings(int[] listOfPlayerIDs)
+        {
+
+            List<List<int>> ExpectedPairings = new List<List<int>>();
+            for (int i = 0; i < listOfPlayerIDs.Length; i += 2)
+            {
+                int firstID = listOfPlayerIDs[i];
+                int secondID = listOfPlayerIDs[i + 1];
+
+                List<int> Pair = new List<int>
+                {
+                    firstID,
+                    secondID
+                };
+                ExpectedPairings.Add(Pair);
+            }
+
+            return ExpectedPairings;
+        }
+    }
+}
